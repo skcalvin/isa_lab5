d1245ac5ed807c433d44d7126c68305ac96f76a6
skcalvin <calvinhuango3o@gmail.com>
2025-10-19 11:55:03 -0700

    missing quote on sql query

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ document.getElementById('insert_sample').addEventListener('click', async () => {
         INSERT into patient (name, dateOfBirth)
         VALUES
             ('Sara Brown', '1901-01-01'),
-            ('John Smith', 1941-01-01'),
+            ('John Smith', '1941-01-01'),
             ('Jack Ma', '1961-01-30'),
             ('Elon Musk', '1999-01-01')
     `;
