3792a885d05395babdea4ea7cfecc55d80d8a355
skcalvin <calvinhuango3o@gmail.com>
2025-10-19 10:05:05 -0700

    client-side code

---
+++ index.js
@@ -0,0 +1,86 @@
+const API_BASE = '';
+const response = document.getElementById('response');
+
+
+
+// Function to load strings
+async function loadStrings() {
+    const data = await fetch('en.json');
+    const strings = await data.json();
+    document.title = strings.index.title;
+    document.getElementById('index_heading').innerText = strings.index.heading;
+    document.getElementById('insert_sample').innerText = strings.index.sampleButton;
+    document.getElementById('query').placeholder = strings.index.queryAreaPlaceholder;
+    document.getElementById('send_query').innerText = strings.index.sendQueryButton;
+    document.getElementById('server_response_heading').innerText = strings.index.serverResponseHeading;
+}
+
+// Function to insert sample data
+document.getElementById('insert_sample').addEventListener('click', async () => {
+    
+    const sql = `
+        INSERT into patient (name, dateOfBirth)
+        VALUES
+            ('Sara Brown', '1901-01-01'),
+            ('John Smith', 1941-01-01'),
+            ('Jack Ma', '1961-01-30'),
+            ('Elon Musk', '1999-01-01')
+    `;
+
+    try {
+        const res = await fetch(API_BASE, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ sql })
+        });
+
+        const data = await res.json();
+        response.textContent = JSON.stringify(data, null, 2);
+    } catch {
+        response.textContent = data.message;
+    }
+});
+
+// Function to send query
+document.getElementById('send_query').addEventListener('click', async () => {
+   
+    const data = await fetch('en.json');
+    const strings = await data.json();
+    
+    const sql = document.getElementById('query').value.trim();
+    try {
+        let res, data;
+
+        if (!sql) {
+            response.textContent = strings.index.missingQuery;
+            return;
+     }
+
+        const lower = sql.toLowerCase();
+
+        if (lower.startsWith('select')) {
+            const encodedSQL = encodeURIComponent(sql);
+            res = await fetch(`${API_BASE}/${encodedSQL}`, { method: "GET" });
+        } else if (lower.startsWith('insert')) {
+            res = await fetch(API_BASE, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ sql })
+            });
+        } else {
+            response.textContent = strings.index.invalidQuery;
+            return;
+        }
+
+        data = await res.json();
+        response.textContent = JSON.stringify(data, null, 2);
+    } catch (err){
+        response.textContent = err.message;
+    }
+});
+
+loadStrings();
